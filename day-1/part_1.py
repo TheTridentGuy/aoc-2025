1f2f1247e8ecacf4b456262e72e229f8d55098da
Aiden Bohlander <aiden.bohlander@gmail.com>
2025-12-02 21:42:43 -0800

    a little washed, but my solution is cleaner this time

--- day-1/part_1.py
+++ day-1/part_1.py
@@ -1,13 +1,14 @@
 with open("input.txt", "r") as f:
     aoc = f.read()
 
+banks = [list(bank) for bank in aoc.split()]
 result = 0
-for id_rng in aoc.split(","):
-    id_rng = id_rng.split("-")
-    for id_ in range(int(id_rng[0]), int(id_rng[1]) + 1):
-        id_ = str(id_)
-        print(len(id_) // 2)
-        if id_[0:len(id_) // 2] == id_[len(id_) // 2:]:
-            result += int(id_)
+
+for bank in banks:
+    print(bank)
+    first = max(bank[:-1], key=lambda x: int(x))
+    first_index = bank.index(first)
+    second = max(bank[first_index:], key=lambda x: int(x))
+    result += int(first+second)
 
 print(result)
