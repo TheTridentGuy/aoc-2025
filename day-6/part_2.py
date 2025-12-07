80a8481fd0ec17f50816a0e9d439e2cf2a437bb4
Aiden Bohlander <aiden.bohlander@gmail.com>
2025-12-06 22:23:02 -0800

    legend has it `day-6/part_2.py` is still running to this day

---
+++ day-6/part_2.py
@@ -0,0 +1,18 @@
+import time
+import functools
+with open("input.txt", "r") as f:
+    aoc_input = f.read()
+result = 0
+aoc_time_start = time.perf_counter()
+##############
+# CODE BELOW #
+##############
+
+
+
+##############
+# CODE ABOVE #
+##############
+print()
+print(result)
+print(f"execution time: {(time.perf_counter()-aoc_time_start)*1000:.2f}ms")
--- day-template/part_1.py
+++ day-template/part_1.py
@@ -8,7 +8,18 @@ aoc_time_start = time.perf_counter()
 # CODE BELOW #
 ##############
 
+rows = aoc_input.split()
+rows = [list(row) for row in rows]
 
+rows[0][rows[0].index("S")] = "|"
+for y in range(1, len(rows)):
+    for x in range(0, len(rows[0])):
+        if rows[y-1][x] == "|":
+            if rows[y][x] == "^":
+                rows[y][x-1], rows[y][x+1] = "|", "|"
+                result += 1
+            else:
+                rows[y][x] = "|"
 
 ##############
 # CODE ABOVE #
